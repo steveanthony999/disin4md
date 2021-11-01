b75cfa028e62c444c37941f21e4b29c3fe1f6820
Steven Woodward <steveanthony999@gmail.com>
2021-11-01 11:17:33 -0700

    initialized sanity

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client';
+
+export default sanityClient({
+  projectId: '0v8ng8yn',
+  dataset: 'production',
+});
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const Data = [
+  {
+    id: 1,
+    title: 'DISSIDENT',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet ligula vel massa dignissim volutpat. Aliquam gravida scelerisque nulla sed laoreet. Mauris nec porta justo.',
+    category: 'T-Shirt',
+    images: {
+      thumbnail: {
+        front: 'thumbnail front location',
+        back: 'thumbnail back location',
+      },
+      full: {
+        front: 'full front location',
+        back: 'full back location',
+        model: 'full model location',
+      },
+    },
+    inventoryandprice: {
+      medium: {
+        qty: 0,
+        price: 29,
+      },
+      large: {
+        qty: 0,
+        price: 29,
+      },
+      xlarge: {
+        qty: 0,
+        price: 29,
+      },
+      xxlarge: {
+        qty: 0,
+        price: 29,
+      },
+    },
+  },
+];
+
+export default Data;
