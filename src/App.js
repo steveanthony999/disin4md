a704ea554927a296d0c51ff3fdddd87bdc206ede
Steven Woodward <steveanthony999@gmail.com>
2021-11-05 22:31:16 -0700

    finished card layout, added hover effect

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route exact path='/'>
             <Home />
           </Route>
-          <Route path='/products'>
+          <Route path='/:id'>
             <Product />
           </Route>
         </Switch>
