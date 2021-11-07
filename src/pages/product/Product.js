410068eb5514c840166ddd68cf0eb84e785b002c
Steven Woodward <steveanthony999@gmail.com>
2021-11-06 22:00:58 -0700

    added data and started styling

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
+import { commerce } from '../../lib/commerce';
+
+import './Product.css';
+
 const Product = () => {
+  const [variant, setVariant] = useState([]);
   const location = useLocation();
   console.log(location.state);
+
+  useEffect(() => {
+    commerce.products
+      .getVariants(location.state.id)
+      .then((variants) => setVariant(variants.data));
+    //   .then(console.log(variant));
+  }, []);
+
   return (
     <div className='Product'>
-      <h1>Product Page {location.state.id}</h1>
-      <img src={location.state.assets[0].url} alt='' width='300px' />
+      <div className='Product-container'>
+        <div className='Product-left'>
+          <img src={location.state.assets[0].url} alt='' width='300px' />
+        </div>
+        <div className='Product-right'>
+          <h1>{location.state.name}</h1>
+          <p>${location.state.price.raw} USD</p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: location.state.description,
+            }}></p>
+        </div>
+      </div>
     </div>
   );
 };
