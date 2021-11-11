b1c607e207b560fe19c1dda95af5cb2d0031a8b2
Steven Woodward <steveanthony999@gmail.com>
2021-11-10 22:01:43 -0800

    added material-ui styling to form

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { Stepper, Step, StepLabel } from '@mui/material';
+import { Stepper, Step, StepLabel, Paper } from '@mui/material';
 
 import AddressForm from '../../components/checkout/AddressForm';
 import PaymentForm from '../../components/checkout/PaymentForm';
+
+import './Checkout.css';
 
 const steps = ['Shipping Address', 'Payment Details'];
 
@@ -15,15 +17,19 @@ const Checkout = () => {
 
   return (
     <div className='Checkout'>
-      <h1>Checkout</h1>
-      <Stepper activeStep={activeStep}>
-        {steps.map((step) => (
-          <Step key={step}>
-            <StepLabel>{step}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      {activeStep === steps.length ? <Confirmation /> : <Form />}
+      <div className='Checkout-container'>
+        <Paper elevation={3}>
+          <h1>Checkout</h1>
+          <Stepper activeStep={activeStep}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
+        </Paper>
+      </div>
     </div>
   );
 };
