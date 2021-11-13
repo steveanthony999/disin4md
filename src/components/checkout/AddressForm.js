1fa374ffa8b839dbed727a00e5d0c988b686b8dc
Steven Woodward <steveanthony999@gmail.com>
2021-11-13 00:17:04 -0800

    setting up address form

--- src/components/checkout/AddressForm.js
+++ src/components/checkout/AddressForm.js
@@ -1,21 +1,78 @@
-import { useForm, FormProvider } from 'react-hook-form';
-
-import FormInput from './FormInput';
+import { useState } from 'react';
 
 const AddressForm = () => {
-  const methods = useForm();
+  const [field, setField] = useState({
+    firstName: '',
+    lastName: '',
+    streetAddress: '',
+    unitNumber: '',
+    city: '',
+    state: '',
+    zip: '',
+  });
 
   return (
     <div className='AddressForm'>
       <h4>Shipping Address</h4>
-      <FormProvider {...methods}>
-        <form onSubmit=''>
-          <div>
-            <FormInput required name='firstName' label='First Name' />
-            <FormInput required name='lastName' label='Last Name' />
-          </div>
-        </form>
-      </FormProvider>
+      <form>
+        {/* FIRST NAME */}
+        <input
+          type='text'
+          required
+          value={field.firstName}
+          onChange={(e) => setField({ ...field, firstName: e.target.value })}
+          placeholder='First name'
+        />
+        {/* LAST NAME */}
+        <input
+          type='text'
+          required
+          value={field.lastName}
+          onChange={(e) => setField({ ...field, lastName: e.target.value })}
+          placeholder='Last Name'
+        />
+        {/* STREET ADDRESS */}
+        <input
+          type='text'
+          required
+          value={field.streetAddress}
+          onChange={(e) =>
+            setField({ ...field, streetAddress: e.target.value })
+          }
+          placeholder='Street Address'
+        />
+        {/* UNIT/APT */}
+        <input
+          type='text'
+          value={field.unitNumber}
+          onChange={(e) => setField({ ...field, unitNumber: e.target.value })}
+          placeholder='Unit/Apt Number'
+        />
+        {/* CITY */}
+        <input
+          type='text'
+          required
+          value={field.city}
+          onChange={(e) => setField({ ...field, city: e.target.value })}
+          placeholder='City'
+        />
+        {/* STATE */}
+        <input
+          type='text'
+          required
+          value={field.state}
+          onChange={(e) => setField({ ...field, state: e.target.value })}
+          placeholder='State'
+        />
+        {/* ZIP */}
+        <input
+          type='text'
+          required
+          value={field.zip}
+          onChange={(e) => setField({ ...field, zip: e.target.value })}
+          placeholder='Zip Code'
+        />
+      </form>
     </div>
   );
 };
--- src/components/checkout/FormInput.js
+++ src/components/checkout/FormInput.js
@@ -4,16 +4,14 @@ import { useFormContext, Controller } from 'react-hook-form';
 const FormInput = ({ name, label, required }) => {
   const { control } = useFormContext();
   return (
-    <div>
-      <Controller
-        as={TextField}
-        control={control}
-        fullWidth
-        name={name}
-        label={label}
-        required={required}
-      />
-    </div>
+    <Controller
+      as={TextField}
+      control={control}
+      fullWidth
+      name={name}
+      label={label}
+      required={required}
+    />
   );
 };
 
